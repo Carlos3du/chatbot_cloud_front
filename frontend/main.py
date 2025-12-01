f535835410e24d5f29df301f423401038a29d9b4
Carlos3du <carlos.mec2005@gmail.com>
2025-12-01 12:12:43 -0300

    fix: front

--- frontend/main.py
+++ frontend/main.py
@@ -1,32 +1,58 @@
-import streamlit as st
+import os
+
 import requests
+import streamlit as st
+from dotenv import load_dotenv
 
-API_url = "https://chatbotcloud.up.railway.app/chat"
+
+def limpar_input():
+    st.session_state["input_message"] = ""
 
 
-st.set_page_config(page_title="IsCoolGPT")
+load_dotenv()
+API_url = os.getenv("API_url")
 
-st.title("IsCoolGPT")
+st.set_page_config(page_title="📚 IsCool GPT")
 
-message = st.text_area(label="input", label_visibility="hidden")
 
-if st.button("Send", type="primary"):
-    if message.strip():
-        try:
-            with st.spinner("Sending..."):
-                response = requests.post(
-                    API_url,
-                    params={"message": message},
-                    timeout=30
-                )
-                
-                if response.status_code == 200:
-                    output = response.json()
-                    st.write(output["response"])
-                else:
-                    st.error(f"Failed to process message: {response.text}")
-        
-        except requests.exceptions.RequestException as e:
-            st.error(f"Conection error: {str(e)}")       
-    else:
-        st.warning("Please type a message") 
+with st.container(key="header"):
+    st.title("📚 IsCool GPT")
+
+
+with st.container(key="user_input"):
+    message = st.text_area(
+        key="input_message",
+        label="input",
+        label_visibility="hidden",
+        placeholder="Digite sua dúvida aqui...",
+    )
+
+
+with st.container(key="buttons", horizontal_alignment="right", horizontal=True):
+    if st.button("Limpar", type="secondary", on_click=limpar_input):
+        st.rerun()
+
+    enviar = st.button("Enviar", type="primary")
+
+
+with st.container(key="chat_output"):
+    if enviar:
+        if message.strip():
+            try:
+                with st.spinner("Enviando..."):
+                    response = requests.post(
+                        API_url, params={"message": message}, timeout=30
+                    )
+
+                    if response.status_code == 200:
+                        output = response.json()
+
+                        st.markdown("###💡 Resposta")
+                        st.write(output["response"])
+                    else:
+                        st.error(f"Falha ao processar mensagem: {response.text}")
+
+            except requests.exceptions.RequestException as e:
+                st.error(f"Erro de conexão: {str(e)}")
+        else:
+            st.warning("Por favor, digite uma mensagem")
